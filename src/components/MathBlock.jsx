7c8e141568ba3f3c90c96601f1d301deecc7a45c
ow3n123 <ow3npoh@gmail.com>
2025-10-22 02:06:38 +1000

    just trying

--- src/components/MathBlock.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'katex/dist/katex.min.css';
-import {BlockMath } from 'react-katex';
-
-export default function MathBlock({ math }) {
-  return <BlockMath math={math} />;
-}
